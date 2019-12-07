ef47b81bcb1473596a478a3afec8e4768491c70f
Ulrik de Muelenaere <ulrikdem@gmail.com>
2019-12-07 20:08:54 +0200

    ranger: Add fzf mappings

---
+++ .config/ranger/commands.py
@@ -0,0 +1,17 @@
+from os.path import abspath
+from ranger.api.commands import Command
+from subprocess import PIPE
+
+class fzf_cd(Command):
+    def execute(self):
+        p = self.fm.execute_command('fd -L0td | fzf --read0 --reverse --prompt="$PWD/"', stdout=PIPE)
+        stdout = p.communicate()[0]
+        if p.returncode == 0:
+            self.fm.cd(abspath(stdout.decode()[:-1]))
+
+class fzf_select(Command):
+    def execute(self):
+        p = self.fm.execute_command('fd -L0 | fzf --read0 --reverse --prompt="$PWD/"', stdout=PIPE)
+        stdout = p.communicate()[0]
+        if p.returncode == 0:
+            self.fm.select_file(abspath(stdout.decode()[:-1]))
