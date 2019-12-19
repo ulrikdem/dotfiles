90301f4a2ce6518dac0e5abf77b9a7de108c0f51
Ulrik de Muelenaere <ulrikdem@gmail.com>
2019-12-19 12:42:49 +0200

    luakit: Add binding to change from HTTPS to HTTP

--- .config/luakit/rc.lua
+++ .config/luakit/rc.lua
@@ -81,6 +81,9 @@ modes.add_binds("normal", {
     {"gs", "Change protocol to HTTPS.", function(win)
         win.view.uri = win.view.uri:gsub("^http:", "https:")
     end},
+    {"gS", "Change protocol to HTTP.", function(win)
+        win.view.uri = win.view.uri:gsub("^https:", "http:")
+    end},
     {"<control-shift-c>", "Copy the selected text.", function()
         luakit.selection.clipboard = luakit.selection.primary
     end},
