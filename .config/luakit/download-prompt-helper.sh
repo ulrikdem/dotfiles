6eba262802621082743ff8a0cedac6d885e775ef
Ulrik de Muelenaere <ulrikdem@gmail.com>
2020-02-21 19:58:26 +0200

    luakit: Also show regular files in download prompt

--- .config/luakit/download-prompt-helper.sh
+++ .config/luakit/download-prompt-helper.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 query=
 while true; do
-    readarray -d '' result < <(cat <(printf '..\0') <(fd --max-depth 1 --follow --type d --print0) |
-        fzf --read0 --reverse --prompt "${PWD%/}/" --query "$query" --expect ctrl-o,tab,shift-tab --print-query --print0 --no-clear)
+    readarray -t result < <(ls --color | fzf --ansi --color hl:3,hl+:3,fg+:-1 --no-clear --reverse \
+        --prompt "${PWD%/}/" --query "$query" --print-query --expect ctrl-p,tab,ctrl-n,ctrl-o)
     ((!${#result[@]})) && exit
     query=
     case ${result[1]} in
+        ctrl-p)
+            cd ..
+            query=${result[0]};;
+        tab)
+            [[ -n ${result[2]} ]] && name=${result[2]} || name=${result[0]}
+            cd -- "$name" 2>/dev/null || query=$name;;
+        ctrl-n)
+            [[ -n ${result[0]} ]] && mkdir -p -- "${result[0]}" && cd -- "${result[0]}";;
         ctrl-o)
             mktemp -t "luakit.XXXXXXXX.$1"
             exit;;
-        tab)
-            [[ -n ${result[2]} ]] && cd -- "${result[2]}" || cd -- "${result[0]}" 2>/dev/null || query=${result[0]};;
-        shift-tab)
-            [[ -n ${result[0]} ]] && mkdir -p -- "${result[0]}" && cd -- "${result[0]}";;
         *)
             [[ -n ${result[0]} ]] && echo "${PWD%/}/${result[0]}" || echo "${PWD%/}/$1"
             exit;;
