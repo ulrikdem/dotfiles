2f4ad0b29a342f81882724175f3f3a4af96ae674
Ulrik de Muelenaere <ulrikdem@gmail.com>
2020-11-18 19:43:27 +0200

    ipython: Disable matplotlib integration when DISPLAY unset

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,4 +1,10 @@
+import os
+
 c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.confirm_exit = False
-c.InteractiveShellApp.pylab = 'tk'
-c.InteractiveShellApp.pylab_import_all = False
+
+if 'DISPLAY' in os.environ:
+    c.InteractiveShellApp.pylab = 'tk'
+    c.InteractiveShellApp.pylab_import_all = False
+else:
+    c.InteractiveShellApp.exec_lines = ['import numpy as np']
