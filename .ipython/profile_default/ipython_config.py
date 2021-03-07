affaf9eff78cf3ae02ea6b344f0b805f621c7951
Ulrik de Muelenaere <ulrikdem@gmail.com>
2021-03-07 18:47:05 +0200

    ipython: Auto import only numpy

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,10 +1,5 @@
 import os
 
 c.TerminalIPythonApp.display_banner = False
+c.InteractiveShellApp.exec_lines = ['import numpy as np']
 c.TerminalInteractiveShell.confirm_exit = False
-
-if 'DISPLAY' in os.environ:
-    c.InteractiveShellApp.pylab = 'tk'
-    c.InteractiveShellApp.pylab_import_all = False
-else:
-    c.InteractiveShellApp.exec_lines = ['import numpy as np']
