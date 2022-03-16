4d10cd7c6c72179c5095edbdf000814bc34cdc53
Ulrik de Muelenaere <ulrikdem@gmail.com>
2022-03-16 12:54:30 +0200

    ipython: Disable vi mode

--- .config/ipython/profile_default/ipython_config.py
+++ .config/ipython/profile_default/ipython_config.py
@@ -1,7 +1,4 @@
 c.TerminalIPythonApp.display_banner = False
 c.InteractiveShellApp.exec_lines = ['import numpy as np']
-
-c.TerminalInteractiveShell.editing_mode = 'vi'
-c.TerminalInteractiveShell.prompt_includes_vi_mode = False
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
 c.TerminalInteractiveShell.confirm_exit = False
