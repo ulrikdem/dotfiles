98792246fe91b4eaf12e772d1345996150547eba
Ulrik de Muelenaere <ulrikdem@gmail.com>
2022-10-30 04:10:52 -0400

    nvidia: Ensure module is unloaded and simplify loading

--- .local/src/nvidia-modules.c
+++ .local/src/nvidia-modules.c
@@ -6,9 +6,9 @@
 int main(int argc, char **argv) {
     setuid(0);
     if (argc == 2 && !strcmp(argv[1], "load")) {
-        return system("/sbin/insmod /usr/lib/modules/$(/bin/uname -r)/extramodules/nvidia.ko.xz && /sbin/modprobe nvidia-drm");
+        return system("/sbin/modprobe -i nvidia && /sbin/modprobe nvidia-drm");
     } else if (argc == 2 && !strcmp(argv[1], "unload")) {
-        return system("/sbin/modprobe -r nvidia-drm");
+        return system("/sbin/modprobe -r nvidia-drm nvidia");
     } else {
         fputs("nvidia-modules load|unload\n", stderr);
         return 1;
