4e05684497f383e2c2f1e60905a43fc2951b12b0
Ulrik de Muelenaere <ulrikdem@gmail.com>
2022-12-01 10:39:54 -0500

    luakit: Remove configuration

--- .config/luakit/download-prompt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-temp=$(mktemp -t luakit.XXXXXXXX)
-alacritty --class xmonad-custom-float -t download \
-    -e "$(dirname "$0")/download-prompt-helper.sh" "$1" "$temp"
-file=$(<"$temp")
-rm -- "$temp"
-[[ -n $file ]] && echo -n "$file" || exit 1
