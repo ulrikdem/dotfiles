5368ac3d0bd521ab124e680c496949fa54e178f9
Ulrik de Muelenaere <ulrikdem@gmail.com>
2023-05-06 16:27:06 -0400

    bin: Update nerd fonts script after changes in v3.0.0

--- .local/share/nerd-fonts/update.sh
+++ .local/share/nerd-fonts/update.sh
@@ -2,6 +2,7 @@
 cd "$(dirname "$0")"
 url=https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/bin/scripts/lib
 wget -nv -O i_all.sh $url/i_all.sh
-for i in $(sed -n 's/,/ /g;s/.*{\(.*\)}.*/\1/p' i_all.sh); do
+source i_all.sh
+for i in "${sets[@]}"; do
     wget -nv -O i_$i.sh $url/i_$i.sh
 done
