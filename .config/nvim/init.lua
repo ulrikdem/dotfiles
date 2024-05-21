7ee2cdf95eba707a3175c2e4195217adf5ff4b7b
Ulrik de Muelenaere <ulrikdem@gmail.com>
2024-05-21 02:39:59 -0400

    nvim: Reduce need for vim. and vim.api.nvim_ prefixes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,6 @@
-vim.cmd.runtime("old_init.vim")
+setmetatable(getfenv(), {__index = vim})
+nvim = setmetatable({}, {__index = function(_, key)
+    return api["nvim_" .. key]
+end})
+
+cmd.runtime("old_init.vim")
