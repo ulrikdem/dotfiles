dc11d3b59d7bee10f0393f417ec093ecc01aef28
Ulrik de Muelenaere <ulrikdem@gmail.com>
2024-05-24 00:31:13 -0400

    nvim: Automatically request and format code lens in Haskell buffers

--- .config/nvim/ftplugin/haskell.lua
+++ .config/nvim/ftplugin/haskell.lua
@@ -1,4 +1,19 @@
 if fn.executable("haskell-language-server-wrapper") ~= 0 and uri_from_bufnr(0):match("^file:") then
+    nvim.create_autocmd({"LspAttach", "CursorHold"}, {
+        buffer = nvim.get_current_buf(),
+        callback = function(ev)
+            local on_codelens = lsp.codelens.on_codelens
+            function lsp.codelens.on_codelens(err, result, ctx, config)
+                for _, lens in ipairs(result) do
+                    lens.command.title = " " .. lens.command.title:gsub("^import%s[^(]*", ""):gsub("%s+", " ")
+                end
+                on_codelens(err, result, ctx, config)
+            end
+            lsp.codelens.refresh({bufnr = ev.buf})
+            lsp.codelens.on_codelens = on_codelens
+        end,
+    })
+
     lsp.start({
         name = "haskell-language-server",
         cmd = {"haskell-language-server-wrapper", "--lsp"},
