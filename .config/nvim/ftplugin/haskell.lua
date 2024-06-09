822e1833fc3854ff4735f2359be50a5fb2525389
Ulrik de Muelenaere <ulrikdem@gmail.com>
2024-06-08 23:08:59 -0400

    nvim: Only refresh Haskell code lenses after LSP attach

--- .config/nvim/ftplugin/haskell.lua
+++ .config/nvim/ftplugin/haskell.lua
@@ -1,37 +1,33 @@
-vim.api.nvim_create_autocmd({"LspAttach", "TextChanged", "InsertLeave"}, {
-    buffer = 0,
-    group = vim.api.nvim_create_augroup("haskell_" .. vim.api.nvim_get_current_buf(), {}),
-    callback = function(ev)
-        local on_codelens = vim.lsp.codelens.on_codelens
-        function vim.lsp.codelens.on_codelens(err, result, ctx, config)
-            local count = #(result or {})
-            if count == 0 then
-                on_codelens(err, result, ctx, config)
-                return
+local function refresh_codelens(ev)
+    local on_codelens = vim.lsp.codelens.on_codelens
+    function vim.lsp.codelens.on_codelens(err, result, ctx, config)
+        local count = #(result or {})
+        if count == 0 then
+            on_codelens(err, result, ctx, config)
+            return
+        end
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        local function resolved(lens)
+            if lens.command then
+                lens.command.title = " " .. lens.command.title:gsub("^import%s[^(]*", ""):gsub("%s+", " ")
             end
-            local client = vim.lsp.get_client_by_id(ctx.client_id)
-            local function resolved(lens)
-                if lens.command then
-                    lens.command.title = " " .. lens.command.title:gsub("^import%s[^(]*", ""):gsub("%s+", " ")
-                end
-                count = count - 1
-                if count == 0 then on_codelens(err, result, ctx, config) end
-            end
-            for i, lens in ipairs(result) do
-                if not lens.command and client then
-                    client.request(vim.lsp.protocol.Methods.codeLens_resolve, lens, function(_, lens)
-                        result[i] = lens
-                        resolved(lens)
-                    end, ctx.bufnr)
-                else
+            count = count - 1
+            if count == 0 then on_codelens(err, result, ctx, config) end
+        end
+        for i, lens in ipairs(result) do
+            if not lens.command and client then
+                client.request(vim.lsp.protocol.Methods.codeLens_resolve, lens, function(_, lens)
+                    result[i] = lens
                     resolved(lens)
-                end
+                end, ctx.bufnr)
+            else
+                resolved(lens)
             end
         end
-        vim.lsp.codelens.refresh({bufnr = ev.buf})
-        vim.lsp.codelens.on_codelens = on_codelens
-    end,
-})
+    end
+    vim.lsp.codelens.refresh({bufnr = ev.buf})
+    vim.lsp.codelens.on_codelens = on_codelens
+end
 
 local root_dir = find_root(
     "hie.yaml",
@@ -62,4 +58,13 @@ start_lsp({
     },
     -- https://haskell-language-server.readthedocs.io/en/stable/configuration.html
     settings = {},
+
+    on_attach = function(_, bufnr)
+        refresh_codelens({buf = bufnr})
+        vim.api.nvim_create_autocmd({"TextChanged", "InsertLeave"}, {
+            buffer = bufnr,
+            group = vim.api.nvim_create_augroup("haskell_" .. bufnr, {}),
+            callback = refresh_codelens,
+        })
+    end,
 })
