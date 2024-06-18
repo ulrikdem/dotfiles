875a6114c22f135700145bebab646551e887b021
Ulrik de Muelenaere <ulrikdem@gmail.com>
2024-06-18 18:50:29 -0400

    nvim: Fix statusline path for narrow windows and dirvish
    The previous implementation would truncate the path in narrow windows,
    even when used for titlestring or tabline. It also didn't use a relative
    path for dirvish buffers (probably due to the trailing slash).
    
    Another effect of this change is that help buffers now show the full
    path. I could add an o.buftype == "help" check for the first branch, but
    that would be less consistent.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -194,12 +194,13 @@ end
 
 function _G.statusline_path(winid, absolute)
     local name = api.nvim_buf_get_name(api.nvim_win_get_buf(winid))
-    if name:match("^term://") then
+    if name == "" then
+        return api.nvim_eval_statusline("%f", {winid = winid}).str
+    elseif name:match("^term://") then
         return name:gsub("^term://.-//%d+:", "term://")
-    elseif name:match("^fugitive://") then
+    else
         return fn.fnamemodify(fn.FugitiveReal(name), absolute and ":~" or ":~:.")
     end
-    return api.nvim_eval_statusline(absolute and "%F" or "%f", {winid = winid}).str
 end
 
 function _G.statusline_git()
