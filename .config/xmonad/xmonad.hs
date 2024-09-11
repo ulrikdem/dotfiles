60d57ba42f08b91a190d6d6377f23109f389c298
Ulrik de Muelenaere <ulrikdem@gmail.com>
2024-09-10 20:32:17 -0400

    Revert "xmonad: Remove checkKeymap, which is broken in 0.18.0"
    This reverts commit e9d852bae52287ed0d2b4eb106cf6dee73568ea2.

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -64,7 +64,7 @@ main = xmonad . conf =<< getTextHeight
 conf textHeight = overrideConfig
     $ withUrgencyHook NoUrgencyHook $ setEwmhActivateHook doAskUrgent $ ewmh
     $ dynamicSBs (statusBar textHeight) $ docks def
-        { startupHook = setDefaultCursor xC_left_ptr
+        { startupHook = setDefaultCursor xC_left_ptr <> checkKeymap (conf textHeight) (keymap textHeight)
         , handleEventHook = workspaceEventHook
         , manageHook = floatManageHook textHeight
         , logHook = floatLogHook
