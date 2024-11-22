7df4b7a4e1020fdd52e4a108a204cd74032a1b50
Ulrik de Muelenaere <ulrikdem@gmail.com>
2024-11-22 00:29:40 -0500

    nvim: Enable formatting bib files with bibtex-tidy

---
+++ .config/nvim/ftplugin/bib.lua
@@ -0,0 +1,14 @@
+-- By default only format (including lowercasing keywords and consistently using curly braces)
+-- and apply reliable small fixes (month abbreviations and dashes in page ranges)
+local formatprg = "bibtex-tidy --blank-lines --no-align --no-remove-dupe-fields --no-escape --curly --months"
+vim.bo.formatprg = formatprg
+
+for cmd, args in pairs({
+    Sort = " --sort",
+    -- Don't sort, and overwrite the first entry with values from later ones, to more easily see the differences
+    Deduplicate = " --remove-dupe-fields --duplicates=key,citation,doi --merge=overwrite",
+}) do
+    vim.api.nvim_buf_create_user_command(0, cmd, function(opts)
+        vim.cmd["!"]({formatprg .. args, range = {opts.line1, opts.line2}})
+    end, {range = "%"})
+end
