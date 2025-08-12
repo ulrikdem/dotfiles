191c6d6022793138b3bbc07db909e8d5f003715f
Ulrik de Muelenaere <ulrikdem@gmail.com>
2025-08-12 11:14:38 -0400

    ipython: Simplify checks for available modules

--- .config/ipython/profile_default/ipython_config.py
+++ .config/ipython/profile_default/ipython_config.py
@@ -1,24 +1,21 @@
+from importlib.util import find_spec
 from pathlib import Path
 import sys
 
 c.TerminalIPythonApp.display_banner = False
+
 c.TerminalInteractiveShell.editing_mode = "vi"
 c.TerminalInteractiveShell.prompt_includes_vi_mode = False
 c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
 c.TerminalInteractiveShell.confirm_exit = False
 
-try:
-    import numpy
+if find_spec("numpy"):
     c.InteractiveShellApp.exec_lines.append("import numpy as np")
-    import matplotlib
+
+if find_spec("matplotlib"):
     c.InteractiveShellApp.exec_lines.append("import matplotlib.pyplot as plt")
     c.InteractiveShellApp.matplotlib = "inline"
-except ModuleNotFoundError:
-    pass
 
 sys.path.append(str(Path(__file__).parent / "extensions"))
-try:
-    import PIL
+if find_spec("PIL"):
     c.InteractiveShellApp.extensions.append("kitty_ipython_ext")
-except ModuleNotFoundError:
-    pass
