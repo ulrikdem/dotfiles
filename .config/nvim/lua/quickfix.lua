d0af0bf9d44bd0bb76641d5b86e02a034693ab47
Ulrik de Muelenaere <ulrikdem@gmail.com>
2026-06-25 23:15:58 -0400

    nvim: Show fold markers after line numbers in quickfix tree

--- .config/nvim/lua/quickfix.lua
+++ .config/nvim/lua/quickfix.lua
@@ -185,7 +185,7 @@ function M.textfunc(args)
                 wo.foldtext = ""
                 wo.foldlevel = tree_foldlevel
                 wo.foldcolumn = "1"
-                wo.numberwidth = 4
+                wo.statuscolumn = "%(%l %)%C "
                 wo.fillchars = "fold: ,foldsep: ,foldinner: ," .. vim.go.fillchars
                 nvim_win_call(list.winid, function() vim.cmd("normal! zv") end)
                 vim.keymap.set("n", "<Left>", "zc", {buf = bufnr})
@@ -194,7 +194,7 @@ function M.textfunc(args)
                 wo.foldtext = "v:lua.require'quickfix'.foldtext()"
                 wo.foldlevel = 1
                 wo.foldcolumn = nil
-                wo.numberwidth = nil
+                wo.statuscolumn = nil
                 wo.fillchars = nil
                 pcall(vim.keymap.del, "n", "<Left>", {buf = bufnr})
                 pcall(vim.keymap.del, "n", "<Right>", {buf = bufnr})
